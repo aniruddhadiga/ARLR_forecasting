b9e0ea67ce799437222d116bc0ebfdc118fa3f1b
aniadiga <aniadiga@gmail.com>
2019-09-30 08:03:13 -0400

    MS_ahd_fct.py changed

--- MS_ahd_fct.py
+++ MS_ahd_fct.py
@@ -1,3 +1,4 @@
+# %load MS_ahd_fct.py
 import sys
 import os
 import pandas as pd
@@ -36,19 +37,18 @@ from ARLR import ARLR_aug_phase, ARLR_red_phase, ARLR_fct, ARLR_err_met, fct_unc
 # Multi-step forecast
 csv_path='data/national/ILINet.csv'
 
-epwk = 51
-yr = 2017
-test_wks = 52
+epwk = 40
+yr = 2016
 win = 208
 ms_fct = 4
-fct_win = 52
+fct_win = 108
 
 llr_tol=1e-2
 allw_lags = np.arange(1,52)
 uncer_anl = False
 Nb = 100
 # Read csv file and create train and test data
-train, test, df, df_train, df_test = data_read_and_prep(csv_path, epwk, yr, test_wks, wght=True, log_tr=True)
+train, test, df, df_train, df_test = data_read_and_prep(csv_path, epwk, yr, fct_win, wght=False, log_tr=True)
 # dates = pd.DatetimeIndex(df_train["DATE"])
 #plt.figure(figsize=(12,7))
 #plt.subplot(2,1,1);plt.plot(train.index,(train));plt.title('Full training data from specified epiweek {}, {}'.format(epwk,yr))
@@ -87,6 +87,8 @@ yp_train.index = ind[:]
 
 # lags to be tested
 obs = train
+yp_fct = np.zeros([ms_fct, fct_win+ms_fct])
+
 for fct_wks in range(0, fct_win):
     
     cffs_arlr = np.zeros([ms_fct,win])
@@ -94,13 +96,14 @@ for fct_wks in range(0, fct_win):
     pred_err = np.zeros([ms_fct,win])
     pred_err= train_pred_err
     fct_prdtrs = obs
-    
-    yp_fct = np.zeros([ms_fct, fct_win])
+    ind_fct = []
     yb_fct = np.zeros([ms_fct, Nb])
+#     ind_fct = []
     for mwks in range(0,ms_fct):
         #range(mwks+1,60)
-        yp_fct[mwks,fct_wks], ind_fct= ARLR_fct(resf,fct_prdtrs,test[(mwks):],lags_app,1)
-        
+        yp_fct[mwks,fct_wks+mwks], ind_fct= ARLR_fct(resf,fct_prdtrs,test[(mwks):],lags_app,1)
+#         ind_fct.append(ind_temp)
+#         pdb.set_trace()
         pred_err = np.append(test[mwks]-yp_fct[mwks],pred_err)
         cffs_arlr[mwks,lags_app-1] = resf.params
         lags_app_fct[mwks,lags_app] = lags_app
@@ -118,3 +121,4 @@ for fct_wks in range(0, fct_win):
     #pdb.set_trace()
     #print(rmse, mae, mape)
     obs = obs.append(pd.Series(test.values[fct_wks], index=test.index[fct_wks:fct_wks+1]))
+
