9198d52c30dd60815c47f39f81d24b2b651a7064
aniadiga <aniadiga@gmail.com>
2019-10-11 10:07:50 -0400

    folder structure modified

---
+++ scripts/ARLR.py
@@ -0,0 +1,282 @@
+'''Contains functions that correspond to the ARLR method'''
+from statsmodels.tsa.ar_model import AR
+import numpy as np
+import statsmodels.api as sm
+#import matplotlib.pyplot as plt
+import pandas as pd
+import pdb, os
+from aw_micro import cdc_data
+import datetime
+# ARLR functions
+def ARLR_aug_phase(train,lags,win,llr_tol=1e-2):
+    y = np.flip(train)
+    y_obs = y[0:win]
+    ind = y_obs.index
+    y_obs = np.array(y_obs.values)
+    lags = list(lags)
+    lags_chk = np.array(lags).astype(int)
+    lags_app = np.array([]).astype(int)
+    err_old = np.linalg.norm(y_obs)#np.random.randn(win,1))
+    tr_tp = np.zeros([win,len(lags)])
+    init_lags_len = len(lags)
+    for k in range(0,init_lags_len):
+        err_m = np.zeros([len(lags_chk)])
+        llr = np.zeros([len(lags_chk)])
+        jj = 0
+        for i in lags_chk:
+#             pdb.set_trace()
+            tr_tp[:,k] = y[i:(win+i)]
+            tr_tp_mul = np.matmul(tr_tp[:,0:(k+1)].T, tr_tp[:,0:(k+1)])
+            res = sm.OLS(y_obs,tr_tp[:,0:(k+1)]).fit()
+            yp = res.predict()
+            err_m[jj] = np.linalg.norm(y_obs-yp)
+            llr[jj] = 2*np.log(err_old/err_m[jj])
+#             pdb.set_trace()
+            jj+=1            
+        imax = np.argmax(llr)
+    #     p_val = chi2.sf(llr[imax],1)
+    #     print(p_val)
+        if llr[imax] >= llr_tol:
+#             pdb.set_trace()
+            tr_tp[:,k] =y[lags_chk[imax]:(win+lags_chk[imax])]
+            lags_app = np.append(lags_app, lags_chk[imax])
+            lags_chk = np.delete(lags_chk, imax)
+            err_old = err_m[imax]
+#             pdb.set_trace()
+        else:
+#             pdb.set_trace()
+            break
+    res = sm.OLS(y_obs,tr_tp[:,0:(k)]).fit()
+    yp = res.predict()
+    return res, yp, y_obs, tr_tp, llr, err_old, lags_app, ind
+
+def ARLR_red_phase(y,tr_tp,err_old, lags, res1, lags_app,ind, llr_tol=1e-2):
+    tot_col = len(lags_app)
+    temp_tr_tp = tr_tp[:,0:tot_col]
+    for i in range(0,tot_col+1):
+        init_lag_len_new = temp_tr_tp.shape[1]
+        jj = 0 
+        err_m = np.zeros([len(lags)])
+        llr = np.zeros([len(lags)])
+        for k in range(0,init_lag_len_new):
+#             pdb.set_trace()
+            res = sm.OLS(y,np.delete(temp_tr_tp,k,1)).fit()
+            yp = res.predict()
+            err_m[jj] = np.linalg.norm(y-yp)
+            llr[jj] = 2*np.log(err_old/err_m[jj])
+            jj+=1
+        imin = np.argmin(llr)
+        if (llr[imin]>llr_tol):
+            temp_tr_tp = np.delete(temp_tr_tp,imin,1)     
+            err_old = err_m[imin]
+#             pdb.set_trace()
+        else:
+#             pdb.set_trace()
+            break
+    res = sm.OLS(y,temp_tr_tp).fit()
+    yp = res.predict()
+    pred_err = y-yp
+    return res, yp, tr_tp, llr, pred_err, res1, lags_app, ind
+
+def ARLR_model(train,allw_lags,win,llr_tol=1e-2):
+    res, yp_aug, y_obs, tr_tp, llr, err_old, lags_app, ind = ARLR_aug_phase(train,allw_lags,win,llr_tol)
+    resf, yp_red, tr_tp, llr, train_pred_err, res1, lags_app, ind = ARLR_red_phase(y_obs,tr_tp, err_old, allw_lags, res,lags_app,ind,llr_tol) # reduction phase
+    yp_train = pd.Series(yp_red)
+    yp_train.index = ind[:]
+    coeffs=np.zeros(np.max(allw_lags)+1)
+    coeffs[lags_app] = resf.params
+    return coeffs, yp_train, tr_tp, llr, train_pred_err, lags_app
+# forecast
+
+def ARLR_fct(coeffs,train,lags_app,fct_win, wks):
+#     pdb.set_trace()
+    lags_app = lags_app[lags_app!=0].astype(int)
+    yp_fct = np.zeros([fct_win,1])
+    pred_err = np.zeros([fct_win,1])
+    y = np.flip(train)
+    # test = np.array(test).reshape(fct_win,1)
+    pred_win = len(coeffs) # coeffs len
+#     y_obs = y[lags_app-1]
+    fct_var = y
+    fct_var = fct_var[lags_app-1]
+    for i in range(0,fct_win):
+#         pdb.set_trace()
+        yp_fct[i] = np.dot(fct_var,coeffs[lags_app])
+        fct_var = np.append(yp_fct[i],fct_var[0:(len(fct_var)-1)])
+        #pred_err[i] = test[i]-yp_fct[i]
+#         pdb.set_trace()
+    #error metrics
+#     pdb.set_trace()
+    #ind = test[(wks-1):(wks-1+fct_win)].index
+    return yp_fct, pred_err
+
+def ARLR_err_met(yp_fct, test):
+    fct_win = len(yp_fct)
+    e = test[0:fct_win]-(yp_fct)
+    RMSE = np.linalg.norm(e)/np.sqrt(fct_win)
+    MAE = np.linalg.norm(e,1)/fct_win
+    MAPE = np.sum(np.divide(np.abs(e),test))/fct_win
+    return RMSE, MAE, MAPE
+
+
+def create_bootstrap(train, pred_err, coeffs, lags_app, win):
+    '''Create bootstrap samples given the prediction error and the AR model'''
+#     pdb.set_trace()
+    lags_app = lags_app[lags_app!=0].astype(int)
+    y = np.flip(train)
+    ind = y.index
+    err_b = np.random.choice(pred_err,win)
+    max_lag = np.max(lags_app).astype('int')    
+    yb_temp = np.zeros((win+max_lag))
+    yb_temp[win:] = y[win:(win+max_lag)]
+    for i in range(win):
+        yb_temp[(win-i-1)] = np.dot(yb_temp[win-(i+1)+lags_app]+err_b[i], coeffs[lags_app])
+#     pdb.set_trace()
+    yb = yb_temp[0:win]
+    ind = ind[0:win]
+#     yp = yb[-1::-1]
+#     ind = ind[-1::-1]
+    return yb, ind
+
+# Bootstrapping 
+def fct_uncert(train, pred_err,coeffs, lags_app, win, Nb=1000):
+    '''Provides Nb forecasts based on the bootstrap samples generated by create_bootstrap fucntion'''
+#     pdb.set_trace()
+    lags_app = lags_app[lags_app!=0].astype(int)
+    yb_mat = np.zeros([Nb,win])
+    yb_fct = np.zeros(Nb)
+    i=0 # discard outlier sample values
+    t_o = 0 # time out variable
+    while i < Nb:
+#         pdb.set_trace()
+        yb_mat[i,:],ind = create_bootstrap(train, pred_err, coeffs, lags_app, win)
+#         pdb.set_trace()
+        yb_fct[i], pred = ARLR_fct(coeffs,yb_mat[i,:],lags_app,1,1)
+        if np.abs(yb_fct[i]) > 100 and t_o<10000:
+            t_o+=1
+            i+=1
+            #pdb.set_trace()
+            continue
+        else:
+            i+=1
+#     pdb.set_trace()
+    return yb_fct
+
+def uncer_scr(yb_fct, yp_fct, ms_fct, N_b, bin_ed):
+    '''Puts the bootstrap samples provided by fct_uncert function into bins with bin edges given by bin_ed'''
+#     be = np.arange(0,n_bins,.1)
+#     be = np.append(be,20)
+    bn_mat = np.zeros([len(bin_ed)-1, ms_fct])
+    log_scr = 0#np.zeros(ms_fct)
+#     pdb.set_trace()
+#         plt.subplot(ms_fct,1,i+1)
+    bn = np.histogram(np.exp(yb_fct[:]),bins=bin_ed)# plt.plot(y_obs)
+    probs = dict(zip(np.round(bn[1],1),bn[0]/N_b))
+    #log_scr = np.log(probs[np.floor(np.exp(test)*10)/10.])
+    bn_mat = bn[0]/N_b
+    return log_scr, bn_mat
+
+def multi_step_fct(data_frame, coeffs, lags_app, train_pred_err, ms_fct, win, Nb, bin_ed, uncer_anl=False):
+    '''Using the data_frame, returns 1, 2,... ms_fct-week ahead forecast and also provides the uncertainty in estimation using bootstrap method if uncer_anl=True'''
+    yp_fct=np.zeros(ms_fct)
+    yb_fct=np.zeros([ms_fct,Nb])
+    log_scr = np.zeros(ms_fct)
+    bn_mat = np.zeros([len(bin_ed)-1, ms_fct])
+    for wks in range(1,ms_fct+1):
+#         pdb.set_trace()
+        yp_fct[wks-1],  err = ARLR_fct(coeffs[wks-1,:],data_frame,lags_app[wks-1,:],1, wks)
+        #train_pred_err[wks-1,:] = np.roll(train_pred_err[wks-1,:],1)# update error vector for uncertainty analy.
+        #train_pred_err[wks-1,0] = data_test[wks-1]-yp_fct[wks-1]
+#         pdb.set_trace()
+        if uncer_anl:
+#             pdb.set_trace()
+            yb_fct[wks-1,:] = fct_uncert(data_frame, train_pred_err[wks-1,:],coeffs[wks-1,:],lags_app[wks-1,:], win, Nb)
+            log_scr[wks-1], bn_mat[:, wks-1] = uncer_scr(yb_fct[wks-1,:], yp_fct[wks-1], ms_fct, Nb, bin_ed)
+        print('Week: {}, Fct: {}, Bs: {}, log_scr: {}'.format(wks, np.exp(yp_fct[wks-1]), np.mean(np.exp(yb_fct[wks-1,:])), log_scr[wks-1]))
+    return yp_fct, yb_fct, log_scr, bn_mat, train_pred_err
+
+def outputdistribution(predictions,bn_mat, bins, region, target, directory, epi_wk):
+    output = pd.DataFrame(columns=['Location', 'Target', 'Type', 'Unit', 'Bin_start_incl', 'Bin_end_notincl', 'Value'])
+    df2 = pd.DataFrame(columns=['Location', 'Target', 'Type', 'Unit', 'Bin_start_incl', 'Bin_end_notincl', 'Value'])
+    df3 = pd.DataFrame(columns=['Location', 'Target', 'Type', 'Unit', 'Bin_start_incl', 'Bin_end_notincl', 'Value'])
+    df4 = pd.DataFrame(columns=['Location', 'Target', 'Type', 'Unit', 'Bin_start_incl', 'Bin_end_notincl', 'Value'])
+    
+    if region.isdigit():
+        df2.loc[0] = ["HHS Region " + region, "Season onset", "Bin", "week", "none", "none", 0.029411765]
+    else:
+        df2.loc[0] = [region,"Season onset", "Bin", "week", "none", "none", 0.029411765]
+
+    for i in range(40,53):
+        if region.isdigit():
+            df2.loc[i-39] = ["HHS Region " + region, "Season onset", "Bin", "week", i, i+1, 0.029411765]
+        else:
+            df2.loc[i-39] = [region, "Season onset", "Bin", "week", i, i+1, 0.029411765]
+    for i in range(1, 21):
+        if region.isdigit():
+            df2.loc[i+14] = ["HHS Region " + region, "Season onset", "Bin", "week", i, i+1, 0.029411765]
+        else:
+            df2.loc[i+14] = [region, "Season onset", "Bin", "week", i, i+1, .0765]
+
+    for i in range(40, 53):
+        if region.isdigit():
+            df3.loc[i-40] = ["HHS Region " + region, "Season peak week", "Bin", "week", i, i+1, 0.03030303]
+        else:
+            df3.loc[i-40] = [region, "Season peak week", "Bin", "week", i, i+1, 0.03030303]
+    for i in range(1, 21):
+        if region.isdigit():
+            df3.loc[i+13] = ["HHS Region " + region, "Season peak week", "Bin", "week", i, i+1, 0.03030303]
+        else:
+            df3.loc[i+13] = [region, "Season peak week", "Bin", "week", i, i+1, 0.03030303]
+
+    for i in range(bn_mat.shape[0]):
+        if region.isdigit():
+            df4.loc[i+1] = ["HHS Region " + region, "Season peak percentage", "Bin", "percent", bins[i], bins[i+1], 0.007633588]
+        else:
+            df4.loc[i+1] = [region, "Season peak percentage", "Bin", "percent", bins[i], bins[i+1], 0.007633588]
+
+    output = output.append(df2)
+    output = output.append(df3)
+    output = output.append(df4)
+
+    for i in range(predictions.shape[0]):
+            df = pd.DataFrame(columns=['Location', 'Target', 'Type', 'Unit', 'Bin_start_incl', 'Bin_end_notincl', 'Value'])
+            hist = bn_mat[:,i]
+            if region.isdigit():
+                df.loc[0] = ["HHS Region " + region, str(i+1) + " wk ahead", "Point", "percent","","", predictions[i]]
+            else:
+                df.loc[0] = [region, str(i+1) + " wk ahead", "Point", "percent","","", predictions[i]]
+    
+            
+            for j in range(hist.shape[0]):
+                if region.isdigit():
+                    df.loc[j+1] = ["HHS Region " + region, str(i+1) + " wk ahead", "Bin", "percent", bins[j], bins[j+1], hist[j]/10]
+                else:
+                    df.loc[j+1] = [region, str(i+1) + " wk ahead", "Bin", "percent", bins[j], bins[j+1], hist[j]/10]
+            
+            
+            output = output.append(df)
+        #pdb.set_trace()
+    #Location Target Type Unit Bin_start_incl Bin_end_notincl Value
+    filename = 'EW' +'{:02}'.format(epi_wk.week) + '_ARLR_' + str(epi_wk.startdate()) + '.csv'
+    if not os.path.isfile(directory+filename):
+        output.to_csv(directory + filename, index=False) 
+    else:
+        output.to_csv(directory + filename, mode='a', index=False, header=False)  
+    return
+    
+def accu_output(predictions, region, accu_dir, ews, st_fips_path):
+    df_s = pd.read_csv(st_fips_path)
+    st_fips_val = df_s[df_s['state_name']==region]['state']
+    pred_state = []
+    output = pd.DataFrame(columns=['date', 'area_id', 'ili'])
+    for i in range(predictions.shape[0]):
+        prediction_date = cdc_data.ew2date(ews.year,ews.week+i)
+        pred_state.append({'date':prediction_date.isoformat(),'area_id':'{:02}'.format(st_fips_val.values[0]),'ili':predictions[i]})
+        df_state = pd.DataFrame(pred_state)
+    output=output.append(df_state)
+    filename = 'prediction-'+prediction_date.strftime('%Y%m%d')+'-state-level.csv'
+    if not os.path.isfile(accu_dir+filename):
+        output.to_csv(accu_dir + filename, index=False) 
+    else:
+        output.to_csv(accu_dir + filename, mode='a', index=False, header=False)  
+    return    
---
+++ scripts/forecast_ARLR.py
@@ -0,0 +1,297 @@
+__version__='1.0.0'
+__processor__='forecast_ARLR'
+
+import sys
+import os
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#import matplotlib.cm as cm
+import epiweeks as epi
+import datetime
+import argparse
+import re
+import shutil
+import pdb
+import configparser
+import argparse
+
+from pandas import Series
+from datetime import date, time, datetime, timedelta
+
+from statsmodels.tsa.ar_model import AR
+from statsmodels.tsa.arima_model import ARMA
+from statsmodels.tsa.arima_model import ARIMA
+
+from sklearn.metrics import mean_squared_error
+
+from scipy.linalg import toeplitz
+from scipy.stats.distributions import chi2
+from scipy import signal
+
+from statsmodels.tsa.stattools import adfuller
+from data_prep import data_read_and_prep, get_season, prepdata
+from ARLR import ARLR_aug_phase, ARLR_red_phase, ARLR_fct, ARLR_err_met, fct_uncert, uncer_scr,multi_step_fct, ARLR_model, outputdistribution, accu_output
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import logging
+log = logging.getLogger(__processor__)
+
+def national():
+    cdcdf = pd.read_csv('data/national/ILINet.csv', header=1)
+    df = cdcdf.drop(["REGION", "REGION TYPE", "AGE 0-4", "AGE 25-49", "AGE 25-64", "AGE 5-24", "AGE 50-64", "AGE 65", "NUM. OF PROVIDERS"], axis=1)
+    
+    df['DATE'] = pd.to_datetime(df.apply(lambda row : epi.Week(int(row["YEAR"]), int(row["WEEK"])).startdate() ,axis=1, result_type='reduce'))
+    
+    return df
+
+def region(number):
+    cdcdf = pd.read_csv('data/regional/ILINet.csv', header=1)
+    cdcdf.drop(["REGION TYPE", "AGE 0-4", "AGE 25-49", "AGE 25-64", "AGE 5-24", "AGE 50-64", "AGE 65", "NUM. OF PROVIDERS"], axis=1, inplace = True)
+    dfs = {}
+    for region in cdcdf["REGION"].unique():
+        dfs[region] = pd.DataFrame(cdcdf.loc[cdcdf['REGION'] == region])
+        
+    for df in dfs.values():
+        df.drop(["REGION"], axis=1, inplace=True)
+        df['DATE'] = pd.to_datetime(df.apply(lambda row : epi.Week(int(row["YEAR"]), int(row["WEEK"])).startdate() ,axis=1, result_type='reduce'))
+        #df.drop(["YEAR", "WEEK"], axis = 1, inplace = True)
+    return dfs["Region " + number]
+
+def state(name):
+    cdcdf = pd.read_csv('data/state/ILINet.csv', header=1)
+    cdcdf = cdcdf.drop(["REGION TYPE", "AGE 0-4", "AGE 25-49", "AGE 25-64", "AGE 5-24", "AGE 50-64", "AGE 65", "NUM. OF PROVIDERS"], axis=1)
+    dfs = {}
+
+    for state in cdcdf["REGION"].unique():
+        dfs[state] = pd.DataFrame(cdcdf.loc[cdcdf["REGION"] == state])
+
+
+    for df in dfs.values():
+        df.drop(["REGION"], axis=1, inplace=True)
+        df['DATE'] = pd.to_datetime(df.apply(lambda row : epi.Week(int(row["YEAR"]), int(row["WEEK"])).startdate() ,axis=1, result_type='reduce'))
+        #df.drop(["YEAR", "WEEK"], axis = 1, inplace = True)
+    
+    return dfs[name]
+
+def get_regions():
+    regions = {"national": national, "1": region, "2": region, "3": region, "4": region, "5": region, "6": region, "7": region, "8": region, "9": region, "10": region, "Alabama": state, "Alaska": state, "Arizona": state, "Arkansas": state, "California": state, "Colorado": state, "Connecticut": state, "Delaware": state, "Georgia": state, "Hawaii": state, "Idaho": state, "Illinois": state, "Indiana": state, "Iowa": state, "Kansas": state, "Kentucky": state, "Louisiana": state, "Maine": state, "Maryland": state, "Massachusetts": state, "Michigan": state, "Minnesota": state, "Mississippi": state, "Missouri": state, "Montana": state, "Nebraska": state, "Nevada": state, "New Hampshire": state, "New Jersey": state, "New Mexico": state, "New York": state, "North Carolina": state, "North Dakota": state, "Ohio": state, "Oklahoma": state, "Oregon": state, "Pennsylvania": state, "Rhode Island": state, "South Carolina": state, "South Dakota": state, "Tennessee": state, "Texas": state, "Utah": state, "Vermont": state, "Virginia": state, "Washington": state, "West Virginia": state, "Wisconsin": state, "Wyoming": state}
+    return regions
+
+def get_targets():
+    targets = {"wili": "% WEIGHTED ILI", "ili": "%UNWEIGHTED ILI", "ilitotal": "ILITOTAL", "totalpatients": "TOTAL PATIENTS"} #onset and peak week still need to be added
+    return targets
+
+def get_bin():    
+    bin_ed= [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10.0, 10.1, 10.2, 10.3, 10.4, 10.5, 10.6, 10.7, 10.8, 10.9, 11.0, 11.1, 11.2, 11.3, 11.4, 11.5, 11.6, 11.7, 11.8, 11.9, 12.0, 12.1, 12.2, 12.3, 12.4, 12.5, 12.6, 12.7, 12.8, 12.9, 13.0, 100]
+    return bin_ed
+ 
+
+def ARLR_module(df, region, target, epi_week, fct_weeks):
+    config = configparser.ConfigParser()
+    config_file = 'config.ini'
+    config.read(config_file) 
+    ews_train = epi_week-1
+    ews_test = epi_week   
+    df_train = df[(df['DATE']<=pd.to_datetime(ews_train.startdate()))]
+    df_train[target] = np.array(df_train[target],float)
+    df_train[target] = df_train[target].replace(0,1e-2) # check if zeros are there in ILI data as we take log
+    df_test = df[(df['DATE']>=pd.to_datetime(ews_train.startdate()))]
+    df_test[target] = np.array(df_test[target],float)
+    df_test[target] = df_test[target].replace(0,1e-2)
+    #targetdf = Series(df[target])
+    #target_series = targetdf[:starttraining_date]
+    #df_train = target_series[:-1]
+    #df_test = target_series[-1:]
+    train = np.log(np.array(df_train[target],'float').astype(float))
+    test = np.log(np.array(df_test[target],'float').astype(float))
+    train = pd.Series(train)
+    train.index = df_train['DATE']
+    test = pd.Series(test)
+    test.index = df_test['DATE']
+    config = configparser.ConfigParser()
+    config_file = 'config.ini'  
+    config.read(config_file)
+    # Multi-step forecast
+    
+    
+    win = int(config['Forecasting']['win']) # Length of the historial training data to be considered
+    
+    fut_wks = int(config['Forecasting']['fut_wks']) # Number of weeks ahead to forecast from training data 
+    ms_fct = fct_weeks # For every forecast week, give additional ms_fct weeks forecast
+    
+    test_win = fut_wks+ms_fct # Number of true value to be fetched (testing accuracy)
+    exp_max_lags =  int(config['Forecasting']['exp_max_lags'])# expected maximum lags to be considered in the model
+    llr_tol=1e-2 # log-likelihood tolerance
+    
+    # Uncertainty analysis
+    uncer_anl = int(config['CDC']['uncer_anl'])
+    Nb = int(config['CDC']['Nb'])
+    # create bins
+    n_bins=int(config['CDC']['n_bins'])
+    
+    #bin_ed = np.arange(0,n_bins,.1)
+    #bin_ed = np.append(bin_ed,20)
+    bin_ed = get_bin()
+    
+    # Read csv file and create train and test data
+    # dates = pd.DatetimeIndex(df_train["DATE"])
+    #plt.figure(figsize=(12,7))
+    #plt.subplot(2,1,1);plt.plot(train.index,(train));plt.title('Full training data from specified epiweek {}, {}'.format(epwk,yr))
+    # plt.subplot(2,1,2);plt.plot((hist_win(train,win)).index,(hist_win(train,win)));plt.title('Training data: 4 year period')
+    
+    # Check data for stationarity in the training data with padding
+    train_win = train[-1:(-win-exp_max_lags-1):-1] # training samples in the window period + buffer
+    
+    
+    #result = adfuller(train_win)
+    #print(result)
+    #if result[1] < 0.05:
+    #    print('p-val of ADF test %e' %result[1])
+    #    print('Stationary signal')
+    # plt.plot(train_win)
+    # Check seasonality
+    #season_ind = get_season(train_win,fft_len=1024,figs=False)
+    # train the model
+    max_lags = 55
+    coeffs=np.zeros([ms_fct,max_lags])
+    train_pred_err=np.zeros([ms_fct, win])
+    yp_train=np.zeros([ms_fct, win]) 
+    lags_app=np.zeros([ms_fct,max_lags])
+    
+    # Train to obtain ARLR coeffs for all specified multi-step forecast:
+    # Ex: For 1-step forecast, consider data from t-1 to t-p for training: ms_fct = 1
+    # for 4-step forecast, consider data for t-4 to t-p for training: ms_fct = 4 
+    # similarly for 1 season, ms_fct = 52
+    for wks in range(1,ms_fct+1):
+        allw_lags = (np.arange(wks,max_lags))
+        coeffs_temp, yp_train[wks-1,:], tr_tp1, llr1, train_pred_err[wks-1,:], lags_temp = ARLR_model(train,allw_lags,win,llr_tol)
+        lags_app[wks-1,lags_temp] = lags_temp
+        coeffs[wks-1,:] = coeffs_temp
+    
+    yp_fct=np.zeros([fut_wks,ms_fct])
+    yb_fct=np.zeros([fut_wks,ms_fct,Nb])
+    log_scr = np.zeros([fut_wks, ms_fct])
+    bn_mat = np.zeros([fut_wks, len(bin_ed)-1, ms_fct])
+    # Once trained, use the coeffs to forecast multi-steps given data frame
+    
+    # For obtaining uncertainty in forecast estimates (using Boot strapping), choose uncer_anl = True,  
+    data_frame = train
+    data_test = []#test
+    for new_wks in np.arange(0,fut_wks):
+        data_frame = data_frame.append(test[new_wks:(new_wks+1)])
+        data_test = data_test[1:]
+        yp_fct[new_wks,:], yb_fct[new_wks,:,:], log_scr[new_wks,:], bn_mat[new_wks, :,:], train_pred_err = multi_step_fct(data_frame, coeffs, lags_app, train_pred_err, ms_fct, win, Nb, bin_ed, uncer_anl)
+    
+    return np.exp(yp_fct), bn_mat
+
+def parse_args():
+    ap = argparse.ArgumentParser(description='ARLR forecasting method for'
+                                 ' state ILI.')
+    ap.add_argument('-b', '--forecast_from', required=True,
+                    help='a date EW format indicating first week to predict.')
+    ap.add_argument('-w', '--weeks', required=True, type=int,
+                    help='number of weeks to predict')
+    ap.add_argument('--out_state', required=False,
+                    help='CSV format output file of state predictions')
+    ap.add_argument('--out_county', required=False,
+                    help='CSV format output file of county predictions')
+    ap.add_argument('--ground_truth', required=False,
+                    help='CSV file ("|") from CDC of state ILI levels')
+    ap.add_argument('--region_type', required=True,
+                    help='national, 1, 2,...,10, state')
+    ap.add_argument('--st_fips', required=False,
+                    help='file of state fips and names')
+    ap.add_argument('--county_ids', required=False,
+                    help='file of all county 5-digit fips')
+    ap.add_argument('--end_date', required=False,
+                    help='date (yyyymmdd) of last ground truth data point')
+    ap.add_argument('--CDC', required=True, default=1,
+                    help='CDC=0 means no uncertainty binning, CDC=1')
+    ap.add_argument('--test', required=False, default=0,
+                    help='test mode, dumps the predictions in folder dump')
+    ap.add_argument('-v', '--verbose',
+                    help='verbose logging', action='store_true')
+    ap.add_argument('-l', '--log', default=None,
+                    help='log file, by default logs are written to'
+                    ' standard output')
+
+    return ap.parse_args()
+
+
+def main():
+    args = parse_args()
+    
+    level = logging.INFO
+    if args.verbose:
+        level = logging.DEBUG
+    log.setLevel(level)
+
+    if args.log is None:
+        handler = logging.StreamHandler()
+    else:
+        handler = logging.FileHandler(args.log)
+
+    log_formatter = logging.Formatter('%(asctime)s:%(levelname)s:'
+                                      '%(name)s.%(funcName)s:%(message)s',
+                                      datefmt='%Y%m%d-%H%M%S')
+    handler.setFormatter(log_formatter)
+    log.addHandler(handler)
+
+    log.info('{} v{}'.format(__processor__,__version__))    
+    
+    
+    regions = get_regions()
+    targets = get_targets()
+    #if args.region not in regions:
+    #    raise TypeError("region is not valid")
+    if args.region_type == "national":
+        args.region_type = "US National"
+    fct_weeks = args.weeks
+    # 
+
+    startyear = args.forecast_from[:4]
+    startweek = args.forecast_from[6:8]
+    trainweek = startweek
+    ews = epi.Week(int(startyear), int(startweek))
+    region_type = args.region_type
+    if args.region_type=='US National' or args.region_type.isdigit():
+        target = targets["wili"]
+    else:
+        target = targets["ili"]
+    csv_path = args.ground_truth
+    fdf = prepdata(csv_path)
+    fdf = fdf.drop(fdf[(fdf['REGION'] == 'Florida') | (fdf['REGION'] == 'Puerto Rico')|(fdf['REGION'] == 'Virgin Islands')|(fdf['REGION'] == 'New York City')].index)
+    
+    if int(args.test):
+        directory = 'dump/'
+        if not os.path.exists(directory):
+            os.makedirs(directory) 
+        
+    else:
+        directory = 'output/' + ARLR + '/'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    bin_ed = get_bin() 
+    
+    for region in fdf['REGION'].unique():
+        #for i in range(0, 1):
+        df = fdf[fdf['REGION']==region]       
+        predictions, bn_mat = ARLR_module(df, region, target, ews, fct_weeks)
+        if int(args.CDC):
+            outputdistribution(predictions[0:4].reshape(4), bn_mat[0:4,:].reshape([131,4]), bin_ed, region, target, directory, ews)
+        if df['REGION TYPE'].unique() == 'States':
+            print(region)
+            accu_output(predictions.reshape(4), region,  args.out_state, ews, args.st_fips)
+if __name__ == "__main__":
+    main()
+# Multi-step forecast
+#    for key in regions.keys():
+#        if key=='Florida':
+#            continue
+#        else:
+#            main({"REGION": key, "TARGET": "ili", "STARTDATE": "2017EW40", "ENDDATE": "2016EW39"})
