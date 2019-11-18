bae0c54ca3a23b3175e5000f62124c0bac363d92
aniadiga <aniadiga@gmail.com>
2019-11-18 05:52:15 -0500

    run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script opens 4 terminal windows.
+
+i=201840
+
+while [ $i -lt 201853 ]
+do
+echo $i
+i=$(($i+1))
+done
+
+j=201901
+while [ $j -lt 201921 ]
+do
+echo $j
+j=$(($j+1))
+done
