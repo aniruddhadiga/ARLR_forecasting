54315bde5ffad3673bdb4225e114206f04c8f16c
aniruddhadiga <aniadiga@gmail.com>
2019-12-08 14:30:05 -0500

    only state exog results

--- dump/run_score.sh
+++ dump/run_score.sh
@@ -1,7 +1,7 @@
 #!/bin/bash/
 year=2018
 sh ../R_load.sh
-varibs=all_state
+varibs=state
 input_filename=${varibs}_exog/${year}/
 score_filename=scores_$((year))_$varibs.csv 
 Rscript score_submission.R $year $input_filename $score_filename
